1f39252ac9e22a92d082ff7943056b113250d830
Abhaya <shankarabhaya9@gmail.com>
2023-07-13 03:46:34 +0530

    stats route complete

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -138,11 +138,21 @@ const showStats = async (req, res) => {
     { $group: { _id: "$status", count: { $sum: 1 } } },
   ]);
 
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
   console.log(stats);
 
-  res
-    .status(StatusCodes.OK)
-    .json({ defaultStats: {}, monthlyApplications: [] });
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] });
 };
 
 module.exports = {
